4a50541cd985cdfc7cbae66b60b403ae0950d5a1
gfjardim <gfjardim@gmail.com>
2015-02-05 19:46:16 -0200

    Fix: popups not closing
    Bump Phusion to 0.9.15
    Fix: popups not closing by itself.

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#########################################
+##        ENVIRONMENTAL CONFIG         ##
+#########################################
+
+# Configure user nobody to match unRAID's settings
+usermod -u 99 nobody
+usermod -g 100 nobody
+usermod -d /home nobody
+chown -R nobody:users /home
+
+# Disable SSH
+rm -rf /etc/service/sshd /etc/my_init.d/00_regen_ssh_host_keys.sh
+
+#########################################
+##    REPOSITORIES AND DEPENDENCIES    ##
+#########################################
+
+# Repositories
+add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ trusty universe multiverse"
+add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ trusty-updates universe multiverse"
+add-apt-repository "deb http://apt.sonarr.tv/ master main"
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
+
+# Install Dependencies
+apt-get update -qq
+apt-get install -qy libgdiplus libmono-cil-dev nzbdrone
+
+#########################################
+##             INSTALLATION            ##
+#########################################
+
+# Fix permissions
+chown -R nobody:users /opt/NzbDrone
+
+#########################################
+##                 CLEANUP             ##
+#########################################
+
+# Clean APT install files
+apt-get clean -y
+rm -rf /var/lib/apt/lists/* /var/cache/* /var/tmp/*
+
